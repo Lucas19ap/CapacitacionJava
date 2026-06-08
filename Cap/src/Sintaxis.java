31307a0a85b485c77a0d2129fca8f8bc7fdd30e7
Lucas19ap <Lucas19ap@gmail.com>
2026-06-08 09:26:49 -0300

    Update Sintaxis.java

--- Cap/src/Sintaxis.java
+++ Cap/src/Sintaxis.java
@@ -1,16 +1,3 @@
-/*
-* Crear un proyecto básico en Java con método Main 
-* Invocar un método que imprima un listado de nombres (Lista de String)
-* Invocar un método que dada una lista de String y un String como parámetro, retorne si la lista contiene el String
- (si al menos un elemento es igual)
-* Invocar un método que dada una lista de String y un String como parámetro, retorne una nueva lista con los elementos de la misma
-que contienen ese String (ejemplo el String "Juan Melo" contiene al String "Melo"); ( Me faltaria este punto )
-* invocar un método que genere un Map (clave Long y valor String).
-  Luego invocar a un método que dado este Map retorne si una clave existe o no
-  Luego otro método que dado ese mismo map retorne el valor de una clave.
-
-*/    
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
@@ -37,10 +24,15 @@ public class Sintaxis {
         alumnosMap.put(35065548L, "Nico");
         alumnosMap.put(25668971L, "Paula");
         alumnosMap.put(24658971L, "Anibal");
-
-        System.out.println("Alumno: " + alumnosMap.get(35051437L) );
+        
+        buscarAlumnoMap(alumnosMap, 35051437L);
 
     }
+
+    private static void buscarAlumnoMap(Map<Long, String> mapa, Long idBuscado) {
+        System.out.println("Alumno: " + mapa.get(idBuscado));
+    }
+
 
     private static void buscarAlumno(List<String> alumnos, String alumnoBuscado) {
         boolean encontrado = false;
@@ -54,7 +46,7 @@ public class Sintaxis {
         }
 
         int i = 0;
-        while (alumnos.get(i) == alumnoBuscado || i < 5) //Acá no supe como poner longitud de la lista (i < 5)
+        while (alumnos.get(i) == alumnoBuscado && i < 5) //Acá no supe como poner longitud de la lista (i < 5)
         {
             i++;
             encontrado = true;
