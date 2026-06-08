3cfaafcffc2a6e27046c3d074f5762d7e9c8f4ec
Lucas19ap <Lucas19ap@gmail.com>
2026-06-08 10:09:56 -0300

    Update Sintaxis.java

--- Cap/src/Sintaxis.java
+++ Cap/src/Sintaxis.java
@@ -1,3 +1,13 @@
+       /* Crear un proyecto básico en Java con método Main
+        * Invocar un método que imprima un listado de nombres (Lista de String)
+        * Invocar un método que dada una lista de String y un String como parámetro, retorne si la lista contiene el String
+        (si al menos un elemento es igual)
+        * Invocar un método que dada una lista de String y un String como parámetro, retorne una nueva lista con los elementos de la misma
+        que contienen ese String (ejemplo el String "Juan Melo" contiene al String "Melo");
+        * invocar un método que genere un Map (clave Long y valor String).
+        Luego invocar a un método que dado este Map retorne si una clave existe o no
+        Luego otro método que dado ese mismo map retorne el valor de una clave. */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
@@ -10,12 +20,12 @@ public class Sintaxis {
 
         List<String> alumnos = new ArrayList<String>();
 
-        alumnos.add("Lucas");
-        alumnos.add("Nico");
-        alumnos.add("Paula");
-        alumnos.add("Anibal");
+        alumnos.add("Lucas A");
+        alumnos.add("Nico E");
+        alumnos.add("Paula A");
+        alumnos.add("Anibal A");
 
-        String alumnoBuscado = "Nico";
+        String alumnoBuscado = "Nico E";
         imprimirAlumnos(alumnos);
         buscarAlumno(alumnos, alumnoBuscado);
 
@@ -24,13 +34,21 @@ public class Sintaxis {
         alumnosMap.put(35065548L, "Nico");
         alumnosMap.put(25668971L, "Paula");
         alumnosMap.put(24658971L, "Anibal");
-        
+        buscarAlumnoList(alumnos);
         buscarAlumnoMap(alumnosMap, 35051437L);
 
     }
 
+    private static void buscarAlumnoList(List<String> alumnos) {
+        for (String alumno : alumnos ) {
+            if (alumno.contains("A")) {
+                System.out.println("Alumno lista: " + alumno);
+            }
+        }
+    }
+
     private static void buscarAlumnoMap(Map<Long, String> mapa, Long idBuscado) {
-        System.out.println("Alumno: " + mapa.get(idBuscado));
+        System.out.println("Alumno en el Mapa: " + mapa.get(idBuscado));
     }
 
 
